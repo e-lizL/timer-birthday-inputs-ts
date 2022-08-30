f719eb3a0f386c1f2bbff383a46ae43ab086d925
Elizabeth Lands <elizabeth_lands@hotmail.com>
2022-08-30 13:58:44 +0100

    first commmit - basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { AppWrapper, FieldWrapper, MessageWrapper, Button } from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+        <h2>Birthday Countdown App</h2>
+      <FieldWrapper>
+        <label htmlFor="name">name:</label>
+        <input type="text" />
+      </FieldWrapper>
+      <FieldWrapper>
+        <label htmlFor="name">next birthday:</label>
+        <input type="text" />
+      </FieldWrapper>
+      <Button>calculate</Button>
+      <MessageWrapper>
+        <div>Eliz, it's your birthday in...</div>
+        <div>birthday calculator</div>
+      </MessageWrapper>
+    </AppWrapper>
   );
 }
 
---
+++ src/styles.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const AppWrapper = styled.div`
+  background: #666;
+  color: white;
+  font-size: 25px; 
+  text-align: center;
+  width: 90%;
+  max-width: 600px;
+  margin: 100px auto;
+  border-radius: 10px;
+  padding: 30px;   
+`
+
+export const FieldWrapper = styled.div`
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  width: 300px;
+  margin: 0 auto; 
+`
+
+export const Button = styled.button`
+  margin: 50px 0;
+  padding: 15px;
+  background: green;
+  color: white;
+  border: 0;
+  border-radius: 10px;
+  font-size: 20px
+`
+
+export const MessageWrapper = styled.div`
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  width: 300px;
+  margin: 0 auto; 
+`
