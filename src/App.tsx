7e3c024235c15a9cadbc057f079778115c8f5f10
Elizabeth Lands <elizabeth_lands@hotmail.com>
2022-10-29 11:15:05 +0100

    feat: change wording on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,74 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { AppWrapper, FieldWrapper, MessageWrapper, Button, MessageHeading, MessageDiv, Input } from './styles';
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  AppWrapper,
+  FieldWrapper,
+  MessageWrapper,
+  Button,
+  MessageHeading,
+  MessageDiv,
+  Input,
+} from "./styles";
 
 function App() {
   const [showMessage, setShowMessage] = useState(false);
-  const [name, setName] = useState('Lenny');
-  const [birthdate, setBirthdate] = useState('03/06/2023');
+  const [name, setName] = useState("Lenny");
+  const [birthdate, setBirthdate] = useState("03/06/2023");
 
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
-  function sortName(event: React.ChangeEvent<HTMLInputElement>){
-    setName(event.target.value)
-  };
+  function sortName(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
 
-  function sortBirthdate(event: React.ChangeEvent<HTMLInputElement>){
-    setBirthdate (event.target.value)
+  function sortBirthdate(event: React.ChangeEvent<HTMLInputElement>) {
+    setBirthdate(event.target.value);
   }
 
   useEffect(() => {
-    const birthday = new Date (birthdate);
+    const birthday = new Date(birthdate);
 
     const interval = setInterval(() => {
       const now = new Date();
       const difference = birthday.getTime() - now.getTime();
-      
+
       const d = Math.floor(difference / (1000 * 60 * 60 * 24));
       setDays(d);
 
-      const h = Math.floor((difference % 1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+      const h = Math.floor(
+        ((difference % 1000) * 60 * 60 * 24) / (1000 * 60 * 60)
+      );
       setHours(h);
 
-      const m = Math.floor((difference % 1000 * 60 * 60) / (1000 * 60));
+      const m = Math.floor(((difference % 1000) * 60 * 60) / (1000 * 60));
       setMinutes(m);
 
-      const s = Math.floor(( difference % 1000 * 60 * 60) / (1000))
+      const s = Math.floor(((difference % 1000) * 60 * 60) / 1000);
       setSeconds(s);
-
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [birthdate])
+  }, [birthdate]);
 
   return (
     <AppWrapper>
-        <h2>Birthday Countdown Timer</h2>
+      <h2>Birthday Countdown Timer</h2>
       <FieldWrapper>
-        <label htmlFor="name">name:</label>
-        <Input
-          type="text"
-          onChange={sortName}
-          autoComplete="off" 
-        />
+        <label htmlFor="name">Your name:</label>
+        <Input type="text" onChange={sortName} autoComplete="off" />
       </FieldWrapper>
       <FieldWrapper>
-        <label htmlFor="name">next birthday:</label>
-        <Input 
-          type="text" 
-          onChange={sortBirthdate}
-          autoComplete="off"
-        />
+        <label htmlFor="name">
+          Your next birthday (please enter in the format month/day/year eg.
+          12/30/2022):
+        </label>
+        <Input type="text" onChange={sortBirthdate} autoComplete="off" />
       </FieldWrapper>
       <Button onClick={() => setShowMessage(true)}>calculate</Button>
-      {showMessage &&
+      {showMessage && (
         <MessageWrapper>
           <MessageHeading>{name}, it's your birthday in...</MessageHeading>
           <MessageDiv>{days} days,</MessageDiv>
@@ -72,7 +76,7 @@ function App() {
           <MessageDiv>{minutes} minutes,</MessageDiv>
           <MessageDiv>and {seconds} seconds.</MessageDiv>
         </MessageWrapper>
-      }
+      )}
     </AppWrapper>
   );
 }
